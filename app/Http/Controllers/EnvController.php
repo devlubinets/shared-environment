d9c279f2b693b860b3800910c1157a8c457561dc
Lubynets Kyrylo <dev.lubinets@gmail.com>
2024-03-21 23:16:56 +0000

    get original file name

--- app/Http/Controllers/EnvController.php
+++ app/Http/Controllers/EnvController.php
@@ -30,7 +30,7 @@ class EnvController extends Controller
     {
         $file = $request->file('env_file');
 //        $env = Env::query()->find("")->first();
-        $env->file_name = $file->getFilename();
+        $env->file_name = $file->getClientOriginalName();
         $env->file_size = 2130120301230;
         $env->save();
         $env->addMedia($file->getRealPath())->toMediaCollection();
