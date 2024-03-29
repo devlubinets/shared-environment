4597c13987f42f9e5b97c29a2d4052453cc962ad
Lubynets Kyrylo <dev.lubinets@gmail.com>
2024-03-29 08:22:12 +0000

    attempt add google sign-in button

--- resources/views/_layouts/parts/authentication/form/email-form.blade.php
+++ resources/views/_layouts/parts/authentication/form/email-form.blade.php
@@ -2,4 +2,5 @@
     @csrf
     <input type="email" name="email">
     <input type="password" name="password">
+    <input type="submit" value="Login">
 </form>
--- resources/views/authentication/login/show.blade.php
+++ resources/views/authentication/login/show.blade.php
@@ -12,12 +12,24 @@
                         <h2>LOGIN</h2>
                     </div>
                     <div class="row pt-4 ps-3 mb-4">
-                        @include('_layouts.parts.authentication.form.email-form')
-                    </div>
-                </div>
-                <div class="col-12 col-md-6">
-                    <div class="splash">
-                        <img src="{{ asset('images/auth/login_splash.webp') }}" alt="splash" class="img-fluid">
+{{--                        @include('_layouts.parts.authentication.form.email-form')--}}
+                        <div id="g_id_onload"
+                             data-client_id="824588110807-32g2b6grasqud47olpv1nam4r5oq5rjp.apps.googleusercontent.com"
+                             data-context="signin"
+                             data-ux_mode="popup"
+                             data-login_uri="http://localhost:45210/api/v1/sign-in/google"
+                             data-nonce=""
+                             data-auto_select="true"
+                             data-itp_support="true">
+                        </div>
+                        <div class="g_id_signin"
+                             data-type="standard"
+                             data-shape="rectangular"
+                             data-theme="outline"
+                             data-text="signin_with"
+                             data-size="large"
+                             data-logo_alignment="left">
+                        </div>
                     </div>
                 </div>
             </div>
